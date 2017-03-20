1787f2773a74b0d716032b2105217197c5eba8ce
brincktron <brincktron@gmail.com>
2017-03-20 16:58:52 +0100

    init

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import scipy.io.wavfile
 import numpy as np
 import matplotlib.pyplot as plt
+import wave
+import sys
 path="p50 male speech 44.1kHz stereo.wav";
 [fs,wavdata]=scipy.io.wavfile.read(path);
-print('test')
+spf = wave.open(path,'r')
+
+#Extract Raw Audio from Wav File
+signal = spf.readframes(-1)
+signal = np.fromstring(signal, 'Int16')
+plt.figure(1)
+plt.title('Signal Wave...')
+plt.plot(signal)
+plt.show()
+print(test)
