6c3306797ce8866b90588ea195fe25c610c80d48
brincktron <brincktron@gmail.com>
2017-03-30 14:51:19 +0200

    version 0.1 working with drag_drop

--- main.py
+++ main.py
@@ -2,31 +2,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import soundfile as sf
+import sys
 
-path1 = "p50 male speech 44.1kHz stereo.wav";
-path2 = "1_sec_silence.wav";
-path3 = "1_sec_440Hz.wav";
+try:
+    path=sys.argv[1]
+except:
+    path = "p50 male speech 44.1kHz stereo.wav"
+    print "No file dragged - using test-wav-file"
 
-sig, fs = sf.read(path1);  # use the Soundfile package to read audio
+sig, fs = sf.read(path)  # use the Soundfile package to read audio
 
 def rms_flat(a):
     """
     Return the root mean square of all the elements of *a*, flattened out.
     from: http://stackoverflow.com/questions/9763471/audioop-rms-why-does-it-differ-from-normal-rms
     """
+    # noinspection PyTypeChecker
     return np.sqrt(np.mean(np.absolute(a) ** 2))
 
 rms_val = rms_flat(sig)
-rms_val_db = 20 * math.log10(rms_val); # Amplitude rato, 20log10
+rms_val_db = 20 * math.log10(rms_val) # Amplitude rato, 20log10
 
 
 print "RMS value: ", str(round(rms_val, 4)), "equals", round(rms_val_db,4), "dBFS";
 print "Peak values", round(sig.max(),4), "and", round(sig.min(),4)
 
-
-# Plot waveform
-x = np.linspace(0, len(sig) / fs, num=len(sig));  # create an x-axis in seconds
+x = np.linspace(0, len(sig) / fs, num=len(sig))  # create an x-axis in seconds
 plt.figure(1)
 plt.title('Signal Wave...')
 plt.plot(x,sig)
 plt.show()
+plt.legend("RMS: ", str(4))
+
+
+
