762782ab180749d82c02dec9345b53eb8f6bc488
brincktron <brincktron@gmail.com>
2017-03-30 16:31:51 +0200

    version 0.11 working with drag_drop

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import math
 import soundfile as sf
 import sys
+import ntpath
 
 try:
     path=sys.argv[1]
@@ -23,16 +24,18 @@ def rms_flat(a):
 rms_val = rms_flat(sig)
 rms_val_db = 20 * math.log10(rms_val) # Amplitude rato, 20log10
 
-
-print "RMS value: ", str(round(rms_val, 4)), "equals", round(rms_val_db,4), "dBFS";
+rms_string="RMS value %s equals %s dBFS" % (str(round(rms_val,4)), round(rms_val_db,4))
+print rms_string
 print "Peak values", round(sig.max(),4), "and", round(sig.min(),4)
 
 x = np.linspace(0, len(sig) / fs, num=len(sig))  # create an x-axis in seconds
 plt.figure(1)
-plt.title('Signal Wave...')
+plt.title(ntpath.basename(path))
 plt.plot(x,sig)
+plt.ylim([-1,1])
+plt.xlim([0,x.max()])
+plt.text(0.1,-0.9, "RMS = %s dBFS" % round(rms_val_db,4))
 plt.show()
-plt.legend("RMS: ", str(4))
 
 
 
